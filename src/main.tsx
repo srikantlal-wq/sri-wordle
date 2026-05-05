047730a7cfbeed568df3b37823c349e068056dd0
srikantlal-wq <srikant.lal@gmail.com>
2026-05-05 12:12:56 +0800

    Ready to deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// @ts-ignore
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
